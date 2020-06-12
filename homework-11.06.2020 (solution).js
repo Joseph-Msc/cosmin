9ae10f5b760c56adf484b084d2756dddb26a2f6b
Avram Cosmin <office@avramdesign.ro>
2020-06-12 17:44:04 +0300

    Update and rename homework.js to homework-11.06.2020 (solution).js

---
+++ homework-11.06.2020 (solution).js
@@ -0,0 +1,21 @@
+let a = true;
+
+if (a === true) {
+    console.log("YES")
+} else {
+    console.log("NO")
+}
+
+a = null;
+
+switch (true) {
+ case b === null:
+     console.log("NULL")
+     break;
+ case b === true:
+     console.log("TRUE")
+     break;
+defult:
+ console.log("NO")
+}
+
