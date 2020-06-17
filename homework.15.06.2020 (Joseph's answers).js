72eeece97449aea034d7865cadac15c835c1b2c4
Joseph Wong <wongj105.319@gmail.com>
2020-06-17 08:29:33 +0100

    Joseph homework answers 17/06/20

---
+++ homework.15.06.2020 (Joseph's answers).js
@@ -0,0 +1,58 @@
+let numbers = [23, 45, 8, 9, 1043, 68]; 
+numbers.push(18)
+let numbersmorethan20 =numbers.filter(function(n){
+    return n > 20;
+});
+
+console.log(numbersmorethan20);
+
+
+var Joseph = {
+    "firstname" : "Joseph",
+    "lastname" : "wong",
+    "age" : 12,
+    "weigth" : "40kg",
+};
+function age(){
+ if (Joseph.age === 12){
+ console.log("Joseph is of age")
+ } else{
+ console.log("Joseph is not of age")
+ }
+};
+ 
+ age("Joseph");
+
+ switch (true){
+    case numbers.length === 7 :
+        console.log("OK")
+        break;
+    case numbers.length === 6 :
+        console.log("Missing the pushed value")
+        break;
+    defult:
+    null
+};
+let a = true;
+
+console.log(btoa("Joseph wong"));
+  if (a === true){
+      console.log(atob("Sm9zZXBoIHdvbmc="));
+  } else {
+      null
+  };
+  
+ function a(){
+console.log("a")
+   b()
+ };
+ function b(){
+     setTimeout(
+         () =>{
+        console.log("e");
+         },
+         0
+     );
+     console.log("c")
+ };
+ console.log("d")
