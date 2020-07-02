72377da42eefecc18746b635fc681b187bb06501
Joseph Wong <wongj105.319@gmail.com>
2020-07-02 17:45:33 +0100

    homework01/07/20

--- homework.15.06.2020 (Joseph's answers).js
+++ homework.15.06.2020 (Joseph's answers).js
@@ -1,18 +1,23 @@
 let numbers = [23, 45, 8, 9, 1043, 68]; 
 numbers.push(18)
-let numbersmorethan20 =numbers.filter(function(n){
+let numbersmorethan20 = numbers.filter(function(n){
     return n > 20;
 });
 
 console.log(numbersmorethan20);
+let Joseph = {};
 
-
-var Joseph = {
+    Joseph = {
     firstname : "Joseph",
     lastname : "wong",
     age : 12,
     weigth : "40kg",
 };
+
+function name(){
+    
+}
+
 function age(age){
  if (age === 12){
  console.log("Joseph is of age")
